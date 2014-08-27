153e9bf9bfec446d0fdcb0d4a747e2bb0de817db
Praveen Murugesan <praveen@uber.com>
2014-08-27 02:09:25 +0000

    fix

--- edit_offset_column.sql
+++ edit_offset_column.sql
@@ -2,12 +2,12 @@ DO
 $BODY$
 DECLARE
   m VARCHAR;
-  arr varchar[] := array['user_sessions', 'trip_client_ratings', 'trip_state_changes', 'trip_driver_ratings', 'trip_fare_splits', 'trips'];
-  --arr varchar[] := array['test'];
+  --arr varchar[] := array['user_sessions', 'trip_client_ratings', 'trip_state_changes', 'trip_driver_ratings', 'trip_fare_splits', 'trips'];
+  arr varchar[] := array['test'];
 BEGIN
   FOREACH m IN ARRAY arr
   LOOP
-        EXECUTE format('update %I set _offset=substring(_offset from %L) || lpad(CAST(substring(_offset, %L) as VARCHAR), 20, %L)', m, '(.*-)', '-(.*)$', '0');
+        EXECUTE format('update %I set _offset=substring(_offset from %L) || lpad(CAST(substring(_offset from %L) as VARCHAR), 20, %L)', m, '(.*-)', '-(.*)$', '0');
   END LOOP;
 END;
 $BODY$ language plpgsql
