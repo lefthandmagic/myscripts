1333db95f47fb2adfa63831164aaecfd61c2991a
Praveen Murugesan <praveen@uber.com>
2014-08-27 07:54:28 +0000

    updates

--- edit_offset_postgres.sql
+++ edit_offset_postgres.sql
@@ -11,4 +11,3 @@ BEGIN
   END LOOP;
 END;
 $BODY$ language plpgsql
-update test set _offset=regexp_substr(_offset,'(.*-)') || lpad(CAST(regexp_substr(_offset, '-(.*)$', 1, 1, '', 1) as VARCHAR) , 20, '0');
